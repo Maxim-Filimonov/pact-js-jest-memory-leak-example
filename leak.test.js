9b68f9fc824f029a46a1a2a7212a915143729b36
Maxim Filimonov <tpaktopsp@gmail.com>
2022-12-23 17:01:15 +0800

    add further investigation analysis

--- leak.test.js
+++ leak.test.js
@@ -2,8 +2,18 @@
 require('@pact-foundation/pact/src/dsl/message')
 require('@pact-foundation/pact/src/dsl/matchers')
 require('@pact-foundation/pact/src/dsl/options')
+require('@pact-foundation/pact/src/common/jsonTypes');
+require('@pact-foundation/pact/src/common/net');
+require('@pact-foundation/pact-core/src/logger/crashMessage');
+require('@pact-foundation/pact-core/src/logger/types');
 // this creates the leak
-require('@pact-foundation/pact/src/dsl/interaction')
+// require('@pact-foundation/pact/src/dsl/interaction')
+// require('@pact-foundation/pact/src/common/request');
+const pino = require('pino')
+const { createLogger } = require('@pact-foundation/pact-core/src/logger/pino');
 
-it('leaks as soon as we require pact', () => {
+
+
+it('leaks', () => {
+  createLogger('info');
 })
--- no_leak.test.js
+++ no_leak.test.js
@@ -1,4 +1,16 @@
+const pino = require('pino')
+const createLogger = () => pino({
+  level: 'info',
+  // looks like this is the culprit
+  // transport: {
+  //   target: 'pino-pretty',
+  // },
+})
 
 it('simple test without leak passes', () => {
   expect(1 + 2).toBe(3)
+})
+it('can use logger', () => {
+  const logger = createLogger();
+  logger.info('test logger via pino')
 })
