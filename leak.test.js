9ff3711a9e87b6cbf0569df33703d918f51c4e52
Maxim Filimonov <tpaktopsp@gmail.com>
2022-12-23 16:41:28 +0800

    add more specific example

--- leak.test.js
+++ leak.test.js
@@ -1,4 +1,9 @@
-require('@pact-foundation/pact')
+// all those have no effect
+require('@pact-foundation/pact/src/dsl/message')
+require('@pact-foundation/pact/src/dsl/matchers')
+require('@pact-foundation/pact/src/dsl/options')
+// this creates the leak
+require('@pact-foundation/pact/src/dsl/interaction')
 
 it('leaks as soon as we require pact', () => {
 })
